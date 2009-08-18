00f7ee4bbd893b76db19e05f48186ad1cd72b9f2
MirraJ2 <MirraJ2@82f6c48a-c420-11dd-bf30-1748d1f6ef2f>
2009-08-18 00:12:25 +0000

    making it so you don't have to scroll to see all the options

--- src/gui/SideDrawnArea.java
+++ src/gui/SideDrawnArea.java
@@ -1,11 +1,11 @@
 package gui;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.LinkedList;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 
@@ -15,10 +15,11 @@ import core.model.SceneNodeSet;
 public class SideDrawnArea extends JComponent {
 
 	public SideDrawnArea(final JinkDocument controller, final SceneNodeSet set) {
-		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		setMaximumSize(new Dimension(200, 10000));
+		setPreferredSize(new Dimension(200, 100));
+		setLayout(new FlowLayout());
 		LinkedList<String> list = set.getNodeNames();
 		for (final String s : list) {
-			add(Box.createVerticalStrut(5));
 			JButton jb = new JButton(s);
 			jb.setAlignmentX(.5f);
 			add(jb);
